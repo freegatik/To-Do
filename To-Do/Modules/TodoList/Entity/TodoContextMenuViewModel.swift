3d758ce5004561fd702a7795c8597772fc614960
freegatik <anton.soloviev2016@gmail.com>
2023-07-19 11:58:00 +0300

    Todo list VIPER core, entities, interactor, presenter, and router

---
+++ To-Do/Modules/TodoList/Entity/TodoContextMenuViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  TodoContextMenuViewModel.swift
+//  To-Do
+//
+//  Created by Anton Solovev on 15.07.2023.
+//
+
+import Foundation
+
+/// Данные для контекстного меню задачи
+struct TodoContextMenuViewModel {
+    let title: String
+    let details: String?
+    let date: String
+    let isCompleted: Bool
+}
+
+enum TodoContextAction {
+    case edit
+    case share
+    case delete
+}
+
+

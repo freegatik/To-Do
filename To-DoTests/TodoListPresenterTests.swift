d6d8015ee7514964fc4dd01428cb5adf347714a8
freegatik <anton.soloviev2016@gmail.com>
2023-07-29 00:41:20 +0300

    Todo list module unit tests and expectations

---
+++ To-DoTests/TodoListPresenterTests.swift
@@ -0,0 +1,462 @@
+//
+//  TodoListPresenterTests.swift
+//  To-DoTests
+//
+//  Created by Anton Solovev on 30.07.2023.
+//
+
+import Foundation
+import Testing
+@testable import To_Do
+
+/// Проверяем поведение презентера списка задач
+@Suite("TodoListPresenter")
+struct TodoListPresenterTests {
+    @Test
+    /// При старте экрана презентер устанавливает заголовок и делает первоначальную загрузку
+    func testViewDidLoadTriggersInitialLoad() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+        presenter.viewDidLoad()
+
+        #expect(view.navigationTitle == "Задачи")
+        #expect(view.isLoadingShown)
+        #expect(interactor.didCallLoadInitial)
+    }
+
+    @Test
+    /// Презентер корректно преобразует доменные модели во view-модели и передает их в представление
+    func testInteractorUpdateRendersViewModels() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let date = Date(timeIntervalSince1970: 0)
+        let items = [
+            TodoItem(id: 1, title: "Task", details: "Details", createdAt: date, isCompleted: false)
+        ]
+
+        presenter.didUpdateTodos(items)
+
+        let viewModel = view.lastViewModels?.first
+        #expect(viewModel?.title == "Task")
+        #expect(viewModel?.details == "Details")
+        #expect(viewModel?.date == DateFormatter.shortFormatter.string(from: date))
+        #expect(viewModel?.isCompleted == false)
+        #expect(view.didDismissContextMenu == true)
+    }
+
+    @Test
+    /// При выборе строки открывается экран редактора в режиме редактирования
+    func testSelectionPushesEditor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let items = [
+            TodoItem(id: 10, title: "Edit me", details: nil, createdAt: Date(), isCompleted: false)
+        ]
+
+        presenter.didUpdateTodos(items)
+        presenter.didSelectItem(at: 0)
+
+        #expect(router.lastMode?.isEdit == true)
+    }
+
+    @Test
+    /// Долгое нажатие вызывает отображение контекстного меню с данными задачи
+    func testLongPressShowsContextMenu() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 5, title: "Share", details: "Details", createdAt: Date(timeIntervalSince1970: 0), isCompleted: false)
+        presenter.didUpdateTodos([item])
+
+        presenter.didLongPressItem(at: 0)
+
+        #expect(view.contextMenuModel?.title == "Share")
+        #expect(view.contextMenuModel?.details == "Details")
+    }
+
+    @Test
+    /// Обработка действия «поделиться» формирует текст и передает его во view
+    func testShareActionSendsTextToView() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 7, title: "Share me", details: "Body", createdAt: Date(timeIntervalSince1970: 0), isCompleted: false)
+        presenter.didUpdateTodos([item])
+        presenter.didLongPressItem(at: 0)
+        presenter.handleContextAction(.share)
+
+        #expect(view.sharedText?.contains("Share me") == true)
+        #expect(view.sharedText?.contains("Body") == true)
+    }
+
+    @Test
+    /// Действие редактирования открывает редактор в режиме edit
+    func testHandleContextActionEditPresentsEditor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 11, title: "Edit", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.didUpdateTodos([item])
+        presenter.didLongPressItem(at: 0)
+
+        presenter.handleContextAction(.edit)
+
+        #expect(router.lastMode?.isEdit == true)
+    }
+
+    @Test
+    /// Удаление через контекстное меню вызывает соответствующий метод интерактора
+    func testHandleContextActionDeleteInvokesInteractor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 12, title: "Remove", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.didUpdateTodos([item])
+        presenter.didLongPressItem(at: 0)
+
+        presenter.handleContextAction(.delete)
+
+        #expect(interactor.deletedItems.count == 1)
+        #expect(interactor.deletedItems.first?.id == 12)
+    }
+
+    @Test
+    /// После исчезновения меню actions игнорируются до следующего выделения
+    func testContextMenuDidDisappearClearsHighlight() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 13, title: "Highlight", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.didUpdateTodos([item])
+        presenter.didLongPressItem(at: 0)
+        presenter.contextMenuDidDisappear()
+
+        presenter.handleContextAction(.delete)
+
+        #expect(interactor.deletedItems.isEmpty)
+    }
+
+    @Test
+    /// handleContextAction не выполняет действий когда highlightedItem nil
+    func testHandleContextActionWhenHighlightedItemIsNilDoesNothing() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        // Не вызываем didLongPressItem, поэтому highlightedItem остается nil
+        let initialRouterCallCount = router.presentEditorCallCount
+        let initialShareCallCount = view.shareCallCount
+        let initialDeleteCallCount = interactor.deletedItems.count
+
+        presenter.handleContextAction(.edit)
+        presenter.handleContextAction(.share)
+        presenter.handleContextAction(.delete)
+
+        // Никакие действия не должны быть вызваны
+        #expect(router.presentEditorCallCount == initialRouterCallCount)
+        #expect(view.shareCallCount == initialShareCallCount)
+        #expect(interactor.deletedItems.count == initialDeleteCallCount)
+    }
+
+    @Test
+    /// didFail записывает ошибку во view и скрывает индикатор загрузки
+    func testDidFailShowsErrorAndStopsLoading() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.didFail(with: DummyError.sample)
+
+        #expect(view.lastErrorMessage == DummyError.sample.localizedDescription)
+        #expect(view.isLoadingShown == false)
+    }
+
+    @Test
+    /// Успешные сценарии редактора инициируют повторную загрузку задач
+    func testEditorFinishRefreshesOnSuccess() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 1, title: "Any", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.todoEditorDidFinish(with: .created(item))
+        presenter.todoEditorDidFinish(with: .updated(item))
+
+        #expect(interactor.refreshCount == 2)
+    }
+
+    @Test
+    /// Отмена в редакторе не вызывает обновления данных
+    func testEditorFinishDoesNothingOnCancel() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.todoEditorDidFinish(with: .cancelled)
+
+        #expect(interactor.refreshCount == 0)
+    }
+
+    @Test
+    /// Обновление поискового запроса пробрасывается в интерактор
+    func testUpdateSearchQueryForwardsToInteractor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.updateSearchQuery("milk")
+
+        #expect(interactor.searchQueries == ["milk"])
+    }
+
+    @Test
+    /// Тоггл completion делегируется интерактору
+    func testToggleCompletionDelegatesToInteractor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 20, title: "Done?", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.didUpdateTodos([item])
+
+        presenter.didToggleCompletion(at: 0)
+
+        #expect(interactor.toggleItems.count == 1)
+        #expect(interactor.toggleItems.first?.id == 20)
+    }
+
+    @Test
+    /// Удаление элемента списка делегируется соответствующему методу интерактора
+    func testDeleteItemDelegatesToInteractor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 30, title: "Delete me", details: nil, createdAt: Date(), isCompleted: false)
+        presenter.didUpdateTodos([item])
+
+        presenter.didDeleteItem(at: 0)
+
+        #expect(interactor.deletedItems.count == 1)
+        #expect(interactor.deletedItems.first?.id == 30)
+    }
+
+    @Test
+    /// Пустой список без поиска показывает дефолтное сообщение
+    func testDidUpdateTodosEmptyShowsDefaultMessage() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.didUpdateTodos([])
+
+        #expect(view.lastEmptyStateMessage == "Задачи отсутствуют.")
+    }
+
+    @Test
+    /// После поиска пустой результат отображает сообщение про отсутствие совпадений
+    func testDidUpdateTodosEmptyAfterSearchShowsQueryMessage() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.updateSearchQuery("milk")
+        presenter.didUpdateTodos([])
+
+        #expect(view.lastEmptyStateMessage == "Ничего не найдено по запросу.")
+    }
+
+    @Test
+    /// Кнопка добавления открывает редактор в режиме создания
+    func testDidTapAddPresentsCreateEditor() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.didTapAdd()
+
+        #expect(router.lastMode?.isCreate == true)
+    }
+
+    @Test
+    /// Текст для шаринга без описания не включает пустую строку
+    func testShareActionWithoutDetailsOmitsEmptyLine() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let date = Date(timeIntervalSince1970: 0)
+        let item = TodoItem(id: 8, title: "Only title", details: nil, createdAt: date, isCompleted: false)
+        presenter.didUpdateTodos([item])
+        presenter.didLongPressItem(at: 0)
+        presenter.handleContextAction(.share)
+
+        let expected = ["Only title", DateFormatter.shortFormatter.string(from: date)].joined(separator: "\n")
+        #expect(view.sharedText == expected)
+    }
+
+    @Test
+    /// Пробельные детали обрезаются до nil при формировании view-модели
+    func testViewModelTrimsDetailsWhitespace() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        let item = TodoItem(id: 3, title: "Task", details: "   ", createdAt: Date(timeIntervalSince1970: 0), isCompleted: false)
+        presenter.didUpdateTodos([item])
+
+        #expect(view.lastViewModels?.first?.details == nil)
+    }
+
+    @Test
+    /// Pull-to-refresh запускает загрузку и включает индикатор
+    func testDidPullToRefreshShowsLoadingAndRefreshes() {
+        let view = MockTodoListView()
+        let interactor = MockTodoListInteractor()
+        let router = MockTodoListRouter()
+        let presenter = TodoListPresenter(view: view, interactor: interactor, router: router)
+
+        presenter.didPullToRefresh()
+
+        #expect(view.isLoadingShown)
+        #expect(interactor.refreshCount == 1)
+    }
+}
+
+/// Тестовый double для вью, собирающий события от презентера
+
+private final class MockTodoListView: TodoListViewProtocol {
+    var navigationTitle: String?
+    var isLoadingShown = false
+    var lastViewModels: [TodoListItemViewModel]?
+    var lastEmptyStateMessage: String?
+    var lastErrorMessage: String?
+    var contextMenuModel: TodoContextMenuViewModel?
+    var didDismissContextMenu = false
+    var sharedText: String?
+    var shareCallCount = 0
+
+    func setNavigationTitle(_ title: String) {
+        navigationTitle = title
+    }
+
+    func showLoading(_ isLoading: Bool) {
+        isLoadingShown = isLoading
+    }
+
+    func showTodos(_ viewModels: [TodoListItemViewModel]) {
+        lastViewModels = viewModels
+    }
+
+    func showEmptyState(message: String) {
+        lastEmptyStateMessage = message
+    }
+
+    func showError(message: String) {
+        lastErrorMessage = message
+    }
+
+    func showContextMenu(for viewModel: TodoContextMenuViewModel) {
+        contextMenuModel = viewModel
+    }
+
+    func dismissContextMenu() {
+        didDismissContextMenu = true
+    }
+
+    func share(text: String) {
+        sharedText = text
+        shareCallCount += 1
+    }
+}
+
+/// Заглушка интерактора, запоминающая вызовы без реализации логики
+private final class MockTodoListInteractor: TodoListInteractorInput {
+    var didCallLoadInitial = false
+    var deletedItems: [TodoItem] = []
+    var toggleItems: [TodoItem] = []
+    var refreshCount = 0
+    var searchQueries: [String] = []
+
+    func loadInitialTodos() {
+        didCallLoadInitial = true
+    }
+
+    func refreshTodos() { refreshCount += 1 }
+
+    func toggleCompletion(for item: TodoItem) { toggleItems.append(item) }
+
+    func deleteTodo(_ item: TodoItem) { deletedItems.append(item) }
+
+    func searchTodos(query: String) { searchQueries.append(query) }
+}
+
+/// Роутер-заменитель для проверки передаваемых режимов открытия редактора
+private final class MockTodoListRouter: TodoListRouterProtocol {
+    var lastMode: TodoEditorMode?
+    var presentEditorCallCount = 0
+
+    func presentEditor(mode: TodoEditorMode, output: TodoEditorModuleOutput) {
+        lastMode = mode
+        presentEditorCallCount += 1
+    }
+}
+
+/// Утилита для удобной проверки режима редактора в тестах
+private extension TodoEditorMode {
+    var isEdit: Bool {
+        if case .edit = self { return true }
+        return false
+    }
+
+    var isCreate: Bool {
+        if case .create = self { return true }
+        return false
+    }
+}
+
+/// Общий форматтер дат для тестов презентера
+private extension DateFormatter {
+    static let shortFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yy"
+        return formatter
+    }()
+}
+
+/// Простейшая ошибка для проверки обработчиков
+private enum DummyError: Error {
+    case sample
+}
+
---
+++ To-DoTests/TodoListTableViewCellTests.swift
@@ -0,0 +1,214 @@
+//
+//  TodoListTableViewCellTests.swift
+//  To-DoTests
+//
+//  Created by Anton Solovev on 30.07.2023.
+//
+
+import XCTest
+@testable import To_Do
+
+/// Проверяем отображение и поведение ячейки списка задач
+@MainActor
+final class TodoListTableViewCellTests: XCTestCase {
+    private var cell: TodoListTableViewCell!
+
+    override func setUp() async throws {
+        try await super.setUp()
+        cell = TodoListTableViewCell(style: .default, reuseIdentifier: nil)
+        cell.frame = CGRect(x: 0, y: 0, width: 320, height: 88)
+        cell.layoutIfNeeded()
+    }
+
+    override func tearDown() async throws {
+        cell = nil
+        try await super.tearDown()
+    }
+
+
+    private func makeEmptyCoder() throws -> NSKeyedUnarchiver {
+        let archiver = NSKeyedArchiver(requiringSecureCoding: false)
+        archiver.encode(0, forKey: "dummy")
+        archiver.finishEncoding()
+        let coder = try NSKeyedUnarchiver(forReadingFrom: archiver.encodedData)
+        coder.requiresSecureCoding = false
+        return coder
+    }
+
+    /// Завершённая задача получает зачёркнутый текст и чекбокс
+    func testConfigureForCompletedTodoAppliesStrikeThroughAndAccessibility() throws {
+        let viewModel = TodoListItemViewModel(
+            id: 1,
+            title: "Написать отчёт",
+            details: "Сдать до 18:00",
+            date: "02.07.2023",
+            isCompleted: true
+        )
+
+        cell.configure(with: viewModel)
+
+        let titleLabel: UILabel = try cell.element(named: "titleLabel")
+        let detailsLabel: UILabel = try cell.element(named: "detailsLabel")
+        let dateLabel: UILabel = try cell.element(named: "dateLabel")
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+
+        let attributed = try XCTUnwrap(titleLabel.attributedText)
+        let strikeValue = attributed.attribute(.strikethroughStyle, at: 0, effectiveRange: nil) as? Int
+        XCTAssertEqual(strikeValue, NSUnderlineStyle.single.rawValue)
+        XCTAssertEqual(statusButton.accessibilityValue, "completed")
+        XCTAssertNotNil(statusButton.image(for: .normal))
+        XCTAssertFalse(detailsLabel.isHidden)
+        XCTAssertEqual(detailsLabel.text, viewModel.details)
+        XCTAssertEqual(dateLabel.text, viewModel.date)
+    }
+
+    /// Активная задача не зачёркивается и скрывает пустое описание
+    func testConfigureForActiveTodoShowsPlainTitleAndHidesDetailsWhenMissing() throws {
+        let viewModel = TodoListItemViewModel(
+            id: 2,
+            title: "Позвонить клиенту",
+            details: nil,
+            date: "05.07.2023",
+            isCompleted: false
+        )
+
+        cell.configure(with: viewModel)
+
+        let titleLabel: UILabel = try cell.element(named: "titleLabel")
+        let detailsLabel: UILabel = try cell.element(named: "detailsLabel")
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+
+        XCTAssertEqual(titleLabel.text, viewModel.title)
+        if let attributed = titleLabel.attributedText, attributed.length > 0 {
+            let strikeValue = attributed.attribute(.strikethroughStyle, at: 0, effectiveRange: nil) as? Int
+            XCTAssertNotEqual(strikeValue, NSUnderlineStyle.single.rawValue, "Active item should not have strikethrough")
+        }
+        XCTAssertTrue(detailsLabel.isHidden)
+        XCTAssertNil(statusButton.image(for: .normal))
+        XCTAssertEqual(statusButton.accessibilityValue, "active")
+    }
+
+    /// prepareForReuse возвращает ячейку в исходное состояние
+    func testPrepareForReuseResetsVisualState() throws {
+        let viewModel = TodoListItemViewModel(
+            id: 3,
+            title: "Закрыть задачу",
+            details: "Удалить из списка",
+            date: "08.07.2023",
+            isCompleted: true
+        )
+        cell.configure(with: viewModel)
+        cell.prepareForReuse()
+
+        let titleLabel: UILabel = try cell.element(named: "titleLabel")
+        let detailsLabel: UILabel = try cell.element(named: "detailsLabel")
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+        let separator: UIView = try cell.element(named: "separatorView")
+
+        XCTAssertNil(titleLabel.attributedText)
+        XCTAssertNil(titleLabel.text)
+        XCTAssertFalse(detailsLabel.isHidden, "prepareForReuse should restore default visibility")
+        XCTAssertNil(statusButton.image(for: .normal))
+        XCTAssertEqual(statusButton.accessibilityValue, nil)
+        XCTAssertFalse(separator.isHidden)
+    }
+
+    /// Флаг separator управляет видимостью разделительной линии
+    func testSetShowsSeparatorUpdatesVisibility() throws {
+        let separator: UIView = try cell.element(named: "separatorView")
+
+        cell.setShowsSeparator(false)
+        XCTAssertTrue(separator.isHidden)
+
+        cell.setShowsSeparator(true)
+        XCTAssertFalse(separator.isHidden)
+    }
+
+    /// Обработчик переключения вызывается при нажатии на статус
+    func testToggleHandlerInvokesClosureOnTap() throws {
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+        var toggleCount = 0
+
+        cell.setToggleHandler {
+            toggleCount += 1
+        }
+
+        statusButton.sendActions(for: .touchUpInside)
+        XCTAssertEqual(toggleCount, 1)
+    }
+
+    /// hitTest расширяет активную область кнопки статуса
+    func testHitTestExpandsStatusButtonTouchArea() throws {
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+        cell.layoutIfNeeded()
+
+        let externalPoint = CGPoint(
+            x: statusButton.frame.minX - 6,
+            y: statusButton.frame.midY
+        )
+        let hitView = cell.hitTest(externalPoint, with: nil)
+
+        XCTAssertTrue(hitView === statusButton, "hitTest should return status button for points slightly outside its bounds")
+    }
+
+    func testInitWithCoderInitializesSubviews() throws {
+        let coder = try makeEmptyCoder()
+        defer { coder.finishDecoding() }
+        let coderCell = try XCTUnwrap(TodoListTableViewCell(coder: coder))
+        coderCell.layoutIfNeeded()
+
+        let _: UILabel = try coderCell.element(named: "titleLabel")
+        let _: UIButton = try coderCell.element(named: "statusButton")
+    }
+
+    /// Когда details есть, detailsLabel.isHidden устанавливается в false
+    func testConfigureWithDetailsShowsDetailsLabel() throws {
+        let viewModel = TodoListItemViewModel(
+            id: 4,
+            title: "Task with details",
+            details: "Some details text",
+            date: "11.07.2023",
+            isCompleted: false
+        )
+
+        cell.configure(with: viewModel)
+
+        let detailsLabel: UILabel = try cell.element(named: "detailsLabel")
+        XCTAssertFalse(detailsLabel.isHidden, "Details label should be visible when details are provided")
+        XCTAssertEqual(detailsLabel.text, "Some details text")
+    }
+
+    /// hitTest возвращает super.hitTest для точек вне расширенной области кнопки
+    func testHitTestReturnsSuperForPointsOutsideButtonArea() throws {
+        cell.layoutIfNeeded()
+        let statusButton: UIButton = try cell.element(named: "statusButton")
+        
+        // Точка внутри bounds ячейки, но далеко от кнопки (справа от кнопки)
+        // Кнопка находится слева (leadingAnchor + 20), поэтому точка справа не должна попадать в расширенную область
+        let farPoint = CGPoint(
+            x: cell.bounds.width - 10,  // Правая часть ячейки
+            y: cell.bounds.midY
+        )
+        let hitView = cell.hitTest(farPoint, with: nil)
+        
+        // Должен вернуться результат super.hitTest (обычно contentView или сама ячейка)
+        XCTAssertNotNil(hitView)
+        XCTAssertFalse(hitView === statusButton, "Points far from button should not hit the button")
+    }
+}
+
+// Вспомогательный доступ к приватным свойствам ячейки
+
+private extension TodoListTableViewCell {
+    func element<T>(named name: String) throws -> T {
+        var mirror: Mirror? = Mirror(reflecting: self)
+        while let current = mirror {
+            if let value = current.children.first(where: { $0.label == name })?.value as? T {
+                return value
+            }
+            mirror = current.superclassMirror
+        }
+        throw NSError(domain: "TodoListTableViewCellTests", code: 0, userInfo: [NSLocalizedDescriptionKey: "Property \(name) not found"])
+    }
+}
+
